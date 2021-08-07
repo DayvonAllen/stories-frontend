edc0835f635576e8adeb6c55c18727827392acb0
DayvonAllen <dayvonallen93@outlook.com>
2021-08-06 23:03:03 -0500

    fixed logout functionality

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,26 +3,17 @@ import cookie from "cookie";
 
 export default async (req, res) => {
   if (req.method === "GET") {
-    // Destroy cookie
-    const backendRes = await fetch(`${API_URL}/auth/logout`);
-
-    if (backendRes.ok) {
-      // Set Cookie
-      res.setHeader(
-        "Set-Cookie",
-        cookie.serialize("token", "", {
-          httpOnly: true,
-          secure: process.env.NODE_ENV !== "development",
-          expires: new Date(0),
-          sameSite: "strict",
-          path: "/",
-        })
-      );
-
-      res.status(200).json({ user: "ok" });
-    } else {
-      res.status(400).json({ message: "Bad Request" });
-    }
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("token", "", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        expires: new Date(0),
+        sameSite: "strict",
+        path: "/",
+      })
+    );
+    res.status(200).json({ user: "ok" });
   } else {
     res.setHeader("Allow", ["GET"]);
     res.status(405).json({ message: `Method ${req?.method} not allowed` });
